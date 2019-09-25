b9ba5c5c81c39e4fa8b5b53b7eed3da7655cf939
Joost van Schaik <joostvanschaik@outlook.com>
2019-09-25 11:11:08 +0200

    Initial version

---
+++ Assets/App/Scripts/SingleShotController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class SingleShotController : MonoBehaviour
+{
+    [SerializeField]
+    private Color _activatedColor = Color.green;
+
+    [SerializeField]
+    private float _resetTime = 0.5f;
+
+    private Color _originalColor;
+
+    private Material _material;
+
+    private float _timeActivated = 0;
+
+    public void ShowActivated()
+    {
+        Debug.Log("Activated");
+        _timeActivated = Time.time;
+    }
+
+    private void Start()
+    {
+        _material = GetComponent<Renderer>().material;
+        _originalColor = _material.color;
+    }
+
+    void Update()
+    {
+        var desiredColor = Time.time - _timeActivated > _resetTime ? _originalColor : _activatedColor;
+        if (_material.color != desiredColor)
+        {
+            _material.color = desiredColor;
+        }
+    }
+}
---
+++ Assets/MixedRealityToolkit.SDK/Features/UX/Interactable/Scripts/Events/ReceiverBase.cs
@@ -0,0 +1,60 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using Microsoft.MixedReality.Toolkit.Input;
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Microsoft.MixedReality.Toolkit.UI
+{
+    /// <summary>
+    /// The base class for all receivers that attach to Interactables
+    /// </summary>
+    public abstract class ReceiverBase
+    {
+        public string Name;
+
+        public bool HideUnityEvents;
+        protected UnityEvent uEvent;
+
+        /// <summary>
+        /// Each Receiver has a base Event it raises, (in addition to others).
+        /// </summary>
+        public UnityEvent Event { get => uEvent; set => uEvent = value; }
+
+        public MonoBehaviour Host;
+
+        /// <summary>
+        /// Constructs an interaction receiver that will raise unity event when triggered.
+        /// </summary>
+        /// <param name="ev">Unity event to invoke. Add more events in deriving class.</param>
+        /// <param name="name">Name of the unity event that will get invoked (visible in editor).</param>
+        public ReceiverBase(UnityEvent ev, String name)
+        {
+            uEvent = ev;
+            Name = name;
+        }
+
+        /// <summary>
+        /// The state has changed
+        /// </summary>
+        public abstract void OnUpdate(InteractableStates state, Interactable source);
+
+        /// <summary>
+        /// A voice command was called
+        /// </summary>
+        public virtual void OnVoiceCommand(InteractableStates state, Interactable source, string command, int index = 0, int length = 1)
+        {
+            // voice command called
+        }
+
+        /// <summary>
+        /// A click event happened
+        /// </summary>
+        public virtual void OnClick(InteractableStates state, Interactable source, IMixedRealityPointer pointer = null)
+        {
+            // click called
+        }
+    }
+}
